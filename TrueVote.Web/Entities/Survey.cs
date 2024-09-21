36e89794d3163945fc026b7cdba61564958c5822
mikerozendo <mikerozendo@gmail.com>
2024-09-21 15:18:11 -0300

    fix: adding name to the Survey's ctor

--- TrueVote.Web/Entities/Survey.cs
+++ TrueVote.Web/Entities/Survey.cs
@@ -2,10 +2,10 @@ using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace TrueVote.Web.Entities;
 
-public sealed class Survey(Guid id, string description, IEnumerable<VoteOption> voteOptions)
+public sealed class Survey(Guid id, string name, string description, IEnumerable<VoteOption> voteOptions)
 {
     public Guid Id { get; set; } = id;
-    public string Name { get; set; }
+    public string Name { get; set; } = name;
     public string Description { get; set; } = description;
     public IEnumerable<VoteOption> VoteOptions { get; } = voteOptions;
     public DateTime? ExpirationDate { get; private set; }
