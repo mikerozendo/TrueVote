e887e2ef8d5de4e050ffc374923659fdfb1ed86f
mikerozendo <mikerozendo@gmail.com>
2024-09-21 16:09:53 -0300

    feat: applying first migration

--- TrueVote.Web/Entities/Survey.cs
+++ TrueVote.Web/Entities/Survey.cs
@@ -2,12 +2,12 @@ using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace TrueVote.Web.Entities;
 
-public sealed class Survey(Guid id, string name, string description, IEnumerable<VoteOption> voteOptions)
+public sealed class Survey(Guid id, string name, string description)
 {
     public Guid Id { get; set; } = id;
     public string Name { get; set; } = name;
     public string Description { get; set; } = description;
-    public IEnumerable<VoteOption> VoteOptions { get; } = voteOptions;
+    public IEnumerable<VoteOption> VoteOptions { get; private set; } = [];
     public DateTime? ExpirationDate { get; private set; }
 
     public void SetExpirationDate(DateTime expirationDate)
